1e7cfb1f9c503bba35f95a0fdc77c562291dc04d
harry34704 <harry34704@outlook.com>
2024-01-13 08:06:21 +0200

    My Refractored code

--- car.py
+++ car.py
@@ -1,10 +1,103 @@
+import unittest 
 from abc import ABC, abstractmethod
+from engine.model.calliope import Calliope
+from engine.model.glissade import Glissade
+from engine.model.palindrome import Palindrome
+from engine.model.rorschach import Rorschach
+from engine.model.thovex import Thovex
+from car import CAR
+from datetime import datetime
+
 
 
 class Car(ABC):
     def __init__(self, last_service_date):
         self.last_service_date = last_service_date
 
+    def record_service(self):
+        self.last_service_date = datetime.now()
+
     @abstractmethod
     def needs_service(self):
         pass
+
+class CapuletEngine(Car, ABC):
+    def __init__(self, last_service_date, current_mileage, last_service_mileage):
+        super().__init__(last_service_date)
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def engine_should_be_serviced(self):
+        return self.current_mileage - self.last_service_mileage > 30000
+
+class SternmanEngine(Car, ABC):
+    def __init__(self, last_service_date, warning_light_is_on):
+        super().__init__(last_service_date)
+        self.warning_light_is_on = warning_light_is_on
+
+    def engine_should_be_serviced(self):
+        return self.warning_light_is_on
+
+class WilloughbyEngine(Car, ABC):
+    def __init__(self, last_service_date, current_mileage, last_service_mileage):
+        super().__init__(last_service_date)
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def engine_should_be_serviced(self):
+        return self.current_mileage - self.last_service_mileage > 60000
+
+class Calliope(CapuletEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+class Glissade(WilloughbyEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+class Palindrome(SternmanEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+class Rorschach(WilloughbyEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+class Thovex(CapuletEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+# Example usage
+if __name__ == "__main__":
+    calliope = Calliope(datetime(2020, 1, 1), 50000, 20000)
+    glissade = Glissade(datetime(2021, 1, 1), 70000, 30000)
+    palindrome = Palindrome(datetime(2019, 1, 1), True)
+    rorschach = Rorschach(datetime(2022, 1, 1), 80000, 40000)
+    thovex = Thovex(datetime(2020, 1, 1), 60000, 25000)
+
+    for car in [calliope, glissade, palindrome, rorschach, thovex]:
+        if car.needs_service():
+            print(f"The {car.__class__.__name__} needs service.")
+        else:
+            print(f"The {car.__class__.__name__} does not need service at the moment.")
+
